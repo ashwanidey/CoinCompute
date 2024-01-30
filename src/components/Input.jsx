74dc7ee7df03ce9384e17724a57c4a046b29b1ff
Ashwani <ashwanidey2904@gmail.com>
2024-01-31 01:17:16 +0530

    Removed AutoComplete from input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ export default function Input({Label,Id,Placeholder,Class,inputVal,handleInput})
     <div className="wrap-input">
         <span className="label-input">{Label}</span>
         <input type="text" id={Id} className={Class}
-        placeholder={Placeholder} onkeypress="return isNumberKey(event)"  value = {inputVal} onChange = {handleInput}/>
+        placeholder={Placeholder} onkeypress="return isNumberKey(event)"  value = {inputVal} onChange = {handleInput} autoComplete="off"/>
     </div>
    
     </>
