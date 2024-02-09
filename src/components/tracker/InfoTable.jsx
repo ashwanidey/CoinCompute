0f421432c9ca17b2d9b783f684986dc213d55e45
Ashwani <ashwanidey2904@gmail.com>
2024-02-10 00:25:31 +0530

    Search Bar added

--- src/components/tracker/InfoTable.jsx
+++ src/components/tracker/InfoTable.jsx
@@ -7,48 +7,57 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
 import { Card } from '@mui/material';
 import { CryptoContext } from '../../context/CryptoContext';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 
 export default function InfoTable() {
-  const {cryptoData} = useContext(CryptoContext);
+  const {cryptoData,searched,requestSearch,setSearchVal,searchVal} = useContext(CryptoContext);
 
   const tableheads = ["Name","Price","Market Cap Change","1H","24H","7D"];
   return (
     <>
-     {cryptoData ? <div className='mt-10 ml-[2rem] mr-[2rem]'>
+    <Paper sx = {{marginBottom:2,padding:2}}>
+        <InputBase
+          value={searchVal}
+          onChange={(e) => setSearchVal(e.target.value)}
+          onCancelSearch={() => cancelSearch()}
+          sx={{backgroundColor:"black",color:"white"}}
+        /> <SearchIcon /></Paper>
+     {cryptoData ? <div >
     <TableContainer component={Card}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650 ,backgroundColor:"white",}}>
         <TableHead>
           <TableRow >
-          <TableCell>Asset</TableCell>
+          <TableCell align="center" sx={{fontWeight:1000}}>Asset</TableCell>
           {tableheads.map((data) => (
-              <TableCell align="right">{data}</TableCell>
+              <TableCell align="center" sx={{fontWeight:1000}}>{data}</TableCell>
             ))}
            
            
           </TableRow>
         </TableHead>
         <TableBody >
-          {cryptoData.map((row) => (
+          {searched.map((row) => (
             <TableRow
               key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 ,justifyContent:"center"} }}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 ,} } }
             >
-              <TableCell component="th" scope="row">
+              <TableCell component="th" align="center">
               <img src = {row.image} className='h-[1.6rem] w-[1.6rem]'/>
               </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.current_price}</TableCell>
-              <TableCell align="right">{(row.market_cap_change_percentage_24h).toFixed(2)}</TableCell>
-              <TableCell align="right">{(row.price_change_percentage_1h_in_currency).toFixed(2)
-}</TableCell>
-<TableCell>{(row.price_change_percentage_24h_in_currency
-).toFixed(2)}</TableCell>
-              <TableCell>{(row.price_change_percentage_7d_in_currency
-).toFixed(2)}</TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">${row.current_price}</TableCell>
+              <TableCell align="center">{(row.market_cap_change_percentage_24h).toFixed(2)}%</TableCell>
+              <TableCell align="center">{(row.price_change_percentage_1h_in_currency).toFixed(2)
+}%</TableCell>
+<TableCell align="center">{(row.price_change_percentage_24h_in_currency
+).toFixed(2) }%</TableCell>
+              <TableCell align="center">{(row.price_change_percentage_7d_in_currency
+).toFixed(2)}%</TableCell>
               
             </TableRow>
           ))}
--- src/components/tracker/Tracker.jsx
+++ src/components/tracker/Tracker.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import InfoTable from './InfoTable'
 import { CryptoProvider } from '../../context/CryptoContext'
+import ControlBar from './ControlBar'
 
 
 const Tracker = () => {
@@ -8,7 +9,10 @@ const Tracker = () => {
     <>
 
     <CryptoProvider>
+      <div className='mt-10 ml-[2rem] mr-[2rem]'>
+    <ControlBar/>
     <InfoTable/>
+    </div>
     </CryptoProvider>
     </>
     
