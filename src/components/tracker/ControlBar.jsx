86ebe26c3f7e692e068c1e56a8a54012d2a758ef
Ashwani <ashwanidey2904@gmail.com>
2024-02-10 12:31:42 +0530

    FullName

--- src/components/tracker/ControlBar.jsx
+++ src/components/tracker/ControlBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ControlBar = () => {
   return (
-    <div className='w-full lg:h-12 h-full lg:border-2 rounded-lg border-solid lg:border-gray-100 border-0 flex lg:flex-row flex-col lg:items-center lg:justify-between relative align-start justify-between'> Ashwani</div>
+    <div className='w-full lg:h-12 h-full lg:border-2 rounded-lg border-solid lg:border-gray-100 border-0 flex lg:flex-row flex-col lg:items-center lg:justify-between relative align-start justify-between'> Ashwani Dey</div>
   )
 }
 
