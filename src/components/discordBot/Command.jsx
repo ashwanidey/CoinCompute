6f7fb92ec8c0a59afb78212d15f5cec26e4157d3
Ashwani <ashwanidey2904@gmail.com>
2024-04-02 13:41:52 +0530

    Updated discord bot

--- src/components/discordBot/Command.jsx
+++ src/components/discordBot/Command.jsx
@@ -6,27 +6,28 @@ import "./bot.css"
 const cards = [
   {
     heading : 'TRADE COMMANDS' , Details : [
-      {commandName : "cc trade", info : "Does something" , aliases : "['tp']", example : "cctp"},
-      {commandName : "cc trade updates", info : "Does something" , aliases : "['tp']", example : "cctp"},
-      {commandName : "cc trade profit", info : "Does something" , aliases : "['tp']", example : "cctp"},
+      {commandName : "cc trade", info : "Starts the trading setup" , aliases : "['t']", example : ["cct"]},
+      {commandName : "cc trade updates", info : "Lists all the existing trade updates" , aliases : "['tu']", example : ["cctu"]},
+      {commandName : "cc trade profit", info : "Lists all the existing trade profits" , aliases : "['tp']", example : ["cctp"]},
     ]
   },
   {
     heading : 'Crypto Commands' , Details : [
-      {commandName : "ccs", info : "Does something" , aliases : "['tp']", example : "cctp"},
-      {commandName : "ccs", info : "Does something" , aliases : "['tp']", example : "cctp"},
-      {commandName : "ccs", info : "Does something" , aliases : "['tp']", example : "cctp"},
+      {commandName : "cc search [symbolName]", info : "Searches the mentioned symbolName or crypto." , aliases : "['s']", example : ["ccs bitcoin" ,"ccs doge"]},
+      {commandName : "cc chart [symbolName]", info : "Shows chart of asset." , aliases : "['c']", example :  ["ccc bitcoin" ,"ccc doge"]},
+      {commandName : "cc price [symbolName]", info : "Shows detail about the crypto." , aliases : "['p']", example : ["ccp btc","ccp doge"]},
     ]
   },
   {
     heading : 'SUB COMMANDS' , Details : [
-      {commandName : "ccs", info : "Does something" , aliases : "['tp']", example : "cctp"},
-      {commandName : "ccs", info : "Does something" , aliases : "['tp']", example : "cctp"},
+      {commandName : "cc quickPrice [symbolName]", info : "Shows quick price of an asset." , aliases : "['q']", example : ["ccq btc","ccq doge"]},
+      {commandName : "cc currency", info : "Shows available fiat." , aliases : "[\"fiat\", \"f\"]", example : ["ccf","ccf inr"]},
+      {commandName : "cc subscribe", info : "Shows an eth address which can be used to donate eth." , aliases : "[\"sub\", \"b\"]", example : ["ccb","ccsub"]},
     ]
   },
   {
     heading : 'UTIL Commands' , Details : [
-      {commandName : "cc help", info : "List all of the available commands" , aliases : "['commands', 'h']", example : "cchelp cch"},
+      {commandName : "cc help", info : "List all of the available commands" , aliases : "['commands', 'h']", example : ["cchelp cch"]},
     ]
   }
 ]
@@ -51,8 +52,13 @@ const Command = () => {
               <Accordion.Header className='text-[1.8rem]'>{detail.commandName}</Accordion.Header>
               <Accordion.Body className='flex flex-col gap-2'>
                 <p className='text-[1rem] font-[500]'>Info: {detail.info}</p>
-                <p className='text-[1rem] font-[500]'>Aliases : <span className='bg-[#f3f4f5] pb-[0.25rem] px-[0.5rem] rounded-sm text-[#4e6e8e]'>{detail.aliases}</span></p>
-                <p className='text-[1rem] font-[500]'>Examples : <span className='bg-[#f3f4f5] pb-[0.25rem] px-[0.5rem] rounded-sm text-[#4e6e8e]'>{detail.example}</span></p>
+                <p className='text-[1rem] font-[500]'>Aliases : <span className='bg-[#f3f4f5] pb-[0.25rem] px-[0.5rem] rounded-sm text-[#233b53]'>{detail.aliases}</span></p>
+                <p className='text-[1rem] font-[500]'>Examples : 
+                {detail.example.map(eg => {
+                  return (
+<span className='bg-[#f3f4f5] pb-[0.25rem] px-[0.5rem] ml-4 rounded-sm text-[#4e6e8e]'>{eg}</span>
+                  )
+                })}</p>
                 
               </Accordion.Body>
             </Accordion.Item>
